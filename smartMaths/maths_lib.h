88f9b5d2126a7f2a528ba069bae5d78d93f28867
Mardelor <remy.zirnheld@telecom-sudparis.eu>
2018-03-01 19:02:10 +0100

    Ajout des fonctions modulo & intersects pour la création du graphe (Non testées pour le moment)

--- smartMaths/maths_lib.h
+++ smartMaths/maths_lib.h
@@ -2,7 +2,46 @@
 // Created by rem on 28/02/18.
 //
 
+#include "Vector.h"
+#include "Circle.h"
+#include <cmath>
+
 #ifndef HERMES_PATHFINDING_MATHS_LIB_H
 #define HERMES_PATHFINDING_MATHS_LIB_H
+
+/** Fonction qui calcul le modulo entre -module/2 et +module/2
+ * @param value la valeur (d'angle par exemple)
+ * @param module
+ */
+double modulo(double value, double module){
+    auto q = (int) (value/module);
+    value = value - q*module;
+    if(value >= module/2){
+        value = value - module;
+    }
+    return value;
+}
+
+/** Renvoie true s'il y a intersection entre le segment et le cercle
+ * @param vector0 vector1 les vecteurs qui délimitent le segment
+ * @param circle le cercle
+ */
+bool intersect(const Vector vector0, const Vector vector1, const Circle circle){
+    // TODO : faire un dessin
+    Vector segment = vector1-vector0;
+    Vector zeroToCenter = circle.getCenter()-vector0;
+    int distance = (int) (zeroToCenter.getRay()*sin(zeroToCenter.getTheta()-segment.getTheta()));
+
+    // Si la distance entre la droite porteuse du segment et le centre du cercle est supérieure au rayon, pas d'intersection
+    if(distance > circle.getRay()){
+        return false;
+    }
+
+    // Sinon on vérifie que le centre du cercle soit dans le triangle définit par l'origine et les deux points délimitant le segment
+    // TODO : Ne fonctionne pas si les vecteurs délimitant le segment sont à x=0...
+    return !((circle.getCenter().getY() < (vector0.getY() / vector0.getX()) * circle.getCenter().getX()) && (circle.getCenter().getY() < (vector1.getY() / vector1.getX()) * circle.getCenter().getX())
+             || (circle.getCenter().getY() > (vector0.getY()/vector0.getX()) * circle.getCenter().getX()) && (circle.getCenter().getY() > (vector1.getY()/vector1.getX())*circle.getCenter().getX()));
+
+}
 
 #endif //HERMES_PATHFINDING_MATHS_LIB_H
