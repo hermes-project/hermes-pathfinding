05c2ac7f0b3de7744a41bbb3709252a7a70bcb0b
Euryale AMBROISE <ambroise.eu@hotmail.fr>
2018-05-11 23:36:52 +0200

    diminution de la taille de la fenetre (tout le monde n'a pas une fenetre aussi énorme)

--- main.cpp
+++ main.cpp
@@ -3,10 +3,14 @@
 
 // Pour le moment c'est un foure-tout pour les tests, histoire de bien apprendre le C++
 // Pour faire le liens entre la partie fonctionnelle et la partie affichage, voir la classe LandmarkFrame
+
+#define XWINDOW (900)
+#define YWINDOW (500)
+
 int main(int argc, char** argv) {
 
     QApplication app(argc, argv);
-    Window window(nullptr, new Landmark(1500, 900, 1));
+    Window window(nullptr, new Landmark(XWINDOW, YWINDOW, 1));
     window.show();
     return app.exec();
 }
